305a3914b57c0ddc646d128ccb183c007ed660b1
rob42 <robert@42.co.nz>
2018-11-01 11:00:05 +1300

    Add correct jolokia jar

--- startpi.sh
+++ startpi.sh
@@ -9,6 +9,8 @@ SIGNALK_HOME=`pwd`
 cd $SIGNALK_HOME
 mkdir -p signalk-static/logs
 
+# jolokia
+JOLOKIA="-javaagent:./hawtio/jolokia-jvm-1.3.4-agent.jar=config=./conf/jolokia.conf"
 EXT="-Djava.util.Arrays.useLegacyMergeSort=true"
 MEM="-Xmx128m -XX:PermSize=128m -XX:MaxPermSize=48m"
 HAWTIO=-Dhawtio.authenticationEnabled=false
@@ -21,4 +23,4 @@ mv signalk-static/logs/start.log signalk-static/logs/start.log.back
 #mvn $EXT $LOG4J exec:java 
 #>>logs/start.log 2>&1 &
 echo "Starting offline: mvn -o -Dexec.args='$EXT' '$LOG4J' '$HAWTIO' exec:java 2>&1" >signalk-static/logs/start.log 2>&1 
-mvn -o -Dexec.args="'$EXT' '$LOG4J' '$HAWTIO'" exec:java >>signalk-static/logs/start.log 2>&1
+mvn -o -Dexec.args="'$EXT' '$LOG4J' '$HAWTIO' '$JOLOKIA'" exec:java >>signalk-static/logs/start.log 2>&1
