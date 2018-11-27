c73776002f07b829072144ce7fde59194bb9869a
rob42 <robert@42.co.nz>
2018-11-27 13:09:01 +1300

    Update jars and java configs

--- startpi.sh
+++ startpi.sh
@@ -10,9 +10,9 @@ cd $SIGNALK_HOME
 mkdir -p signalk-static/logs
 
 # jolokia
-JOLOKIA="-javaagent:./hawtio/jolokia-jvm-1.3.4-agent.jar=config=./conf/jolokia.conf"
+JOLOKIA="-javaagent:./hawtio/jolokia-jvm-1.6.0-agent.jar=config=./conf/jolokia.conf"
 EXT="-Djava.util.Arrays.useLegacyMergeSort=true"
-MEM="-Xmx128m -XX:PermSize=128m -XX:MaxPermSize=48m"
+MEM="-Xmx128m -XX:+HeapDumpOnOutOfMemoryError -Dio.netty.leakDetection.level=ADVANCED -XX:+UseParallelGC -XX:+AggressiveOpts"
 HAWTIO=-Dhawtio.authenticationEnabled=false
 LOG4J=-Dlog4j.configuration=file://$SIGNALK_HOME/conf/log4j2.json
 
